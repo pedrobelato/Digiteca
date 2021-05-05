a9fc9beda79331e2c4c129fa1b7b39c6fee7e20b
Pedro <pedrohbelato.ph52@gmail,com>
2021-05-05 13:21:35 -0300

    Inserção da classe usuário

--- Digiteca/Model/Usuario.cs
+++ Digiteca/Model/Usuario.cs
@@ -8,6 +8,27 @@ namespace Digiteca.Model
 {
     public class Usuario
     {
+        private int _id;
+        private string _nome;
+        private string _cpf;
+        private string _telefone;
+        private string _logradouro;
 
+        public Usuario(){}
+
+        public Usuario(int id, string nome, string cpf, string telefone, string logradouro)
+        {
+            _id = id;
+            _nome = nome;
+            _cpf = cpf;
+            _telefone = telefone;
+            _logradouro = logradouro;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Cpf { get => _cpf; set => _cpf = value; }
+        public string Telefone { get => _telefone; set => _telefone = value; }
+        public string Logradouro { get => _logradouro; set => _logradouro = value; }
     }
 }
