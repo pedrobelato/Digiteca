db7f2ba542a6b1e12184e9cd60081c0cf8a90973
Matheus Mioto <matheus_mioto@hotmail.com>
2021-05-19 20:10:21 -0300

    Criado EditoraDAL

--- Digiteca/DAL/EditoraDAL.cs
+++ Digiteca/DAL/EditoraDAL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Digiteca.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,23 @@ namespace Digiteca.DAL
     public class EditoraDAL
     {
         MySQLPersistencia _banco = new MySQLPersistencia();
+
+        public List<Editora> ObterPorID(int id)
+        {
+            List<Editora> editora = new List<Editora>();
+            string sql = $"select * from editora where codEditora = '{id}'";
+            _banco.AbrirConexao();
+            DataTable _dados = _banco.ExecutarSelect(sql);
+            for (int i = 0; i < _dados.Rows.Count; i++)
+            {
+                editora.Add(new Editora(Convert.ToInt32(_dados.Rows[i]["codEditora"]),
+                            _dados.Rows[i]["editora"].ToString(),
+                            _dados.Rows[i]["endereco"].ToString(),
+                            _dados.Rows[i]["telefone"].ToString(),
+                            _dados.Rows[i]["email"].ToString()));
+            }
+            _banco.FecharConexao();
+            return editora;
+        }
     }
 }
