e3faca88993a29989ae07b1ea393530791e496ff
Matheus Mioto <matheus_mioto@hotmail.com>
2021-05-19 20:28:38 -0300

    Criado ItemEmprestimoDAL

--- Digiteca/DAL/ItemEmprestimoDAL.cs
+++ Digiteca/DAL/ItemEmprestimoDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Digiteca.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,62 @@ namespace Digiteca.DAL
 {
     class ItemEmprestimoDAL
     {
+        MySQLPersistencia _banco = new MySQLPersistencia();
 
+        public (int, string) GravarItemEmprestimo(ItemEmprestimo itememprestimo)
+        {
+            int linhasAfetadas = 0;
+            string msg = "";
+            try
+            {
+                string sql = "insert into itememprestimo (codEmprestimo, codUsuario, codExemplar, dataEmp, dataLimite, devolucao) values (@codEmp, @codUsu, @codExe, @dataEmp, @sdataLimite, @dev)";
+
+                Dictionary<string, object> ps = new Dictionary<string, object>();
+                ps.Add("@codEmprestimo", itememprestimo.CodEmprestimo);
+                ps.Add("@codUsuario", itememprestimo.CodUsuario);
+                ps.Add("@codExemplar", itememprestimo.CodExemplar);
+                ps.Add("@dataEmp", itememprestimo.DataEmp);
+                ps.Add("@dataLimite", itememprestimo.DataLimite);
+                ps.Add("@devolucao", itememprestimo.Devolucao);
+
+                _banco.AbrirConexao();
+                linhasAfetadas = _banco.ExecutarNonQuery(sql, ps);
+                _banco.FecharConexao();
+            }
+            catch
+            {
+                msg = "Não foi possível salvar! Tente novamente.";
+            }
+            return (linhasAfetadas, msg);
+        }
+
+
+        public (int, string) Atualizar(ItemEmprestimo itememprestimo)
+        {
+            int linhasAfetadas = 0;
+            string msg = "";
+            try
+            {
+                string sql = "update itememprestimo set codUsuario = @codUsu, codExemplar = @codExe, dataEmp = @dataemp, dataLimite = @datali, devolucao = @dev where codEmprestimo = @cod";
+
+                Dictionary<string, object> ps = new Dictionary<string, object>();
+                ps.Add("@codEmprestimo", itememprestimo.CodEmprestimo);
+                ps.Add("@codUsuario", itememprestimo.CodUsuario);
+                ps.Add("@codExemplar", itememprestimo.CodExemplar);
+                ps.Add("@dataEmp", itememprestimo.DataEmp);
+                ps.Add("@dataLimite", itememprestimo.DataLimite);
+                ps.Add("@devolucao", itememprestimo.Devolucao);
+
+                _banco.AbrirConexao();
+                linhasAfetadas = _banco.ExecutarNonQuery(sql);
+                _banco.FecharConexao();
+            }
+            catch
+            {
+                msg = "Não foi possível salvar! Tente novamente.";
+            }
+
+            return (linhasAfetadas, msg);
+        }
     }
 }
